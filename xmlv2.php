65ead689219cd705f69d2fee8bb7775cb7f79143
Marycielo29 <marycielomb29@gmail.com>
2025-12-11 09:38:01 -0500

    .

--- xmlv2.php
+++ xmlv2.php
@@ -1,90 +1,101 @@
 <?php
 
+//CONEXION
 $conexion = new mysqli("localhost", "root", "root", "sigi_huanta");
 if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 }
 
+//INICIO
 $xml = new DOMDocument('1.0', 'UTF-8');
 $xml->formatOutput = true;
 
+//CREACION DE DOCUMENTO XML
 $et1 = $xml->createElement('programas_estudio');
 $xml->appendChild($et1);
 
-//CONSULTAS
 $consulta = "SELECT * FROM sigi_programa_estudios";
 $resultado = $conexion->query($consulta);
-while($pe = mysqli_fetch_assoc($resultado)) { 
-    echo $pe ['nombre']."<br>";
-    //generar las etiquetas de programas
-    $num_pe = $xml->createElement('pe_'.$pe['id']); //  numeracion de `programas de estudio
-    $codigo_pe = $xml->createElement('codigo', $pe['codigo']); // crear
-    $tipo_pe = $xml->createElement('tipo', $pe['tipo']);
-    $nombre_pe = $xml->createElement('nombre', $pe['nombre']);
+while ($pe = mysqli_fetch_assoc($resultado)) {
+    echo $pe['nombre'] . "<br>";
+    $num_pe = $xml->createElement('pe_' . $pe['id']);
+    $codigo_pe = $xml->createElement('codigo', $pe['codigo']);
     $num_pe->appendChild($codigo_pe);
+    $tipo_pe = $xml->createElement('tipo', $pe['tipo']);
     $num_pe->appendChild($tipo_pe);
+    $nombre_pe = $xml->createElement('nombre', $pe['nombre']);
     $num_pe->appendChild($nombre_pe);
-    //planes de estudios
+    //plan
     $et_plan = $xml->createElement('planes_estudio');
-    $consulta_plan = "SELECT * FROM sigi_planes_estudio WHERE id_programa_estudios=".$pe['id'];
+    $consulta_plan = "SELECT * FROM sigi_planes_estudio WHERE id_programa_estudios=" . $pe['id'];
     $resultado_plan = $conexion->query($consulta_plan);
     while ($plan = mysqli_fetch_assoc($resultado_plan)) {
-         echo $plan ['nombre']."<br>";
-         //generar las etiquetas de planes
-         $num_pe = $xml->createElement('resutalado_plan_'.$plan['id']);
-         $programa_plan = $xml->createElement('id_programa_estudios', $plan['id_programa_estudios']);
-         $num_pe->appendChild($programa_plan);
-         $nombre_plan = $xml->createElement('nombre', $plan['nombre']);
-         $num_pe->appendChild($nombre_plan);
-         $resolucion_plan = $xml->createElement('resolucion', $plan['resolucion']);
-         $num_pe->appendChild($resolucion_plan);
-         $fecha_plan = $xml->createElement('fecha_resgitro', $plan['fecha_registro']);
-         $num_pe->appendChild($fecha_plan);
-         
-         //modulos
-         $et_modulos = $xml->createElement('modulos');
-         $consulta_modulos = "SELECT * FROM sigi_modulo_formativo WHERE id_plan_estudio=".$plan['id'];
-         $resultado_modulos = $conexion->query($consulta_modulos);
-         while ($modulo = mysqli_fetch_assoc($resultado_modulos)) {
-            echo $modulo ['descripcion']."<br>";
-            //generar las etiquetas de planes
-            $num_pe = $xml->createElement('resultado_modulos'.$modulo['id']);
-            $descripcion_modulo = $xml->createElement('descripcion', $modulo['descripcion']);
-            $num_pe->appendChild($descripcion_modulo);
-            $numero = $xml->createElement('nro_modulo', $modulo['nro_modulo']);
-            $num_pe->appendChild($numero);
-            $plan = $xml->createElement('id_plan_estudio', $modulo['id_plan_estudio']);
-            $num_pe->appendChild($plan);
+        echo "--" . $plan['nombre'] . "<br>";
+        $num_plan = $xml->createElement('plan_' . $plan['id']);
+        $nombre_plan = $xml->createElement('nombre', $plan['nombre']);
+        $num_plan->appendChild($nombre_plan);
+        $resolucion_plan = $xml->createElement('resolucion', $plan['resolucion']);
+        $num_plan->appendChild($resolucion_plan);
+        $fecha_registro_plan = $xml->createElement('fecha_registro', $plan['fecha_registro']);
+        $num_plan->appendChild($fecha_registro_plan);
 
-            //semestre
-            $et_semestres = $xml->createElement('modulos');
-            $consulta_semestres = "SELECT * FROM sigi_semestre WHERE id_modulo_formativo=".$pe['id'];
-            $resultado_semestres = $conexion->query($consulta_semestres);
-            while ($semestre = mysqli_fetch_assoc($resultado_semestres)) {
-                echo $semestre ['descripcion']."<br>";
-                $num_pe = $xml->createElement('resultado_semestres'.$semestre['id']);
-                $descripcion_semestre = $xml->createElement('descripcion', $semestre['descripcion']);
-                $num_pe->appendChild($descripcion_semestre);
-                $modulo_formativo = $xml->createElement('id_modulo_formativo', $semestre['id_modulo_formativo']);
-                $num_pe->appendChild($modulo_formativo);
+        //modulos
+        $et_modulos = $xml->createElement('modulos_formativos');
+        $consulta_mod = "SELECT * FROM sigi_modulo_formativo WHERE id_plan_estudio=" . $plan['id'];
+        $resultado_mod = $conexion->query($consulta_mod);
+        while ($modulo = mysqli_fetch_assoc($resultado_mod)) {
+            echo "----" . $modulo['descripcion'] . "<br>";
+            $num_modulo = $xml->createElement('modulo_' . $modulo['id']);
+            $descripcion_mod = $xml->createElement('descripcion', $modulo['descripcion']);
+            $num_modulo->appendChild($descripcion_mod);
+            $nro_modulo_mod = $xml->createElement('nro_modulo', $modulo['nro_modulo']);
+            $num_modulo->appendChild($nro_modulo_mod);
 
-                //unidades didacticas
-                $et_unidades = $xml->createElement('modulos');
-                $consulta_unidades = "SELECT * FROM sigi_unidad_didactica WHERE id_semestre=".$pe['id'];
-                $resultado_unidades = $conexion->query($consulta_unidades);
-                while ($unidades = mysqli_fetch_assoc($resultado_unidades)) {
-                    echo $unidades ['nombre']."<br>";
-                    $num_pe = $xml->createElement('resultado_unidades'.$unidades['id']);
+            //periodos
+            $et_periodos = $xml->createElement('periodos');
+            $consulta_per = "SELECT * FROM sigi_semestre WHERE id_modulo_formativo=" . $modulo['id'];
+            $resultado_per = $conexion->query($consulta_per);
+            while ($per = mysqli_fetch_assoc($resultado_per)) {
+                echo "------" . $per['descripcion'] . "<br>";
+                $num_per = $xml->createElement('periodo_' . $per['id']);
+                $descripcion_per = $xml->createElement('descripcion', $per['descripcion']);
+                $num_per->appendChild($descripcion_per);
+
+                //Unidades Didacticas
+                $et_uds = $xml->createElement('unidades_didacticas');
+                $consulta_uds = "SELECT * FROM sigi_unidad_didactica WHERE id_semestre=" . $per['id'];
+                $resultado_uds = $conexion->query($consulta_uds);
+                while ($uds = mysqli_fetch_assoc($resultado_uds)) {
+                    echo "--------" . $uds['nombre'] . "<br>";
+                    $num_ud = $xml->createElement('ud_' . $uds['orden']);
+                    $nombre_ud = $xml->createElement('nombre', $uds['nombre']);
+                    $num_ud->appendChild($nombre_ud);
+                    $creditos_teorico = $xml->createElement('creditos_teorico', $uds['creditos_teorico']);
+                    $num_ud->appendChild($creditos_teorico);
+                    $creditos_practico = $xml->createElement('creditos_practico', $uds['creditos_practico']);
+                    $num_ud->appendChild($creditos_practico);
+                    $tipo = $xml->createElement('tipo', $uds['tipo']);
+                    $num_ud->appendChild($tipo);
+                    $hr_semanal = ($uds['creditos_teorico']*1)+($uds['creditos_practico']*2);
+                    $hr_sem = $xml->createElement('horas_semanal', $hr_semanal);
+                    $num_ud->appendChild($hr_sem);
+                    $hr_semestral = $xml->createElement('horas_semestral', $hr_semanal*16);
+                    $num_ud->appendChild($hr_semestral);
+                    $et_uds->appendChild($num_ud);
                 }
+                $num_per->appendChild($et_uds);
+                $et_periodos->appendChild($num_per);
             }
-         }
+            $num_modulo->appendChild($et_periodos);
+            $et_modulos->appendChild($num_modulo);
+        }
+        $num_plan->appendChild($et_modulos);
+        $et_plan->appendChild($num_plan);
     }
-
     $num_pe->appendChild($et_plan);
     $et1->appendChild($num_pe);
-
 }
 
+// FINAL
 $archivo = "ies_db.xml";
 $xml->save($archivo);
-?>
---
+++ xmlv3.php
@@ -0,0 +1,42 @@
+<?php
+$xml = simplexml_load_file('ies_db.xml') or die ('ERROR: NO SE CARGO EL XML. ESCRIBE CORRECTAMENTE EL NOMBRE DEL ARCHIVO');
+
+//ACCEDER A LOS ELEMENTOS
+//echo $xml->pe_1->nombre."<br>"; 
+//echo $xml->pe_2->nombre;
+
+foreach ($xml as $i_pe => $pe) {
+    //imprimir los nombres de PE
+    echo 'Codigo: '.$pe->codigo.'<br>';
+    echo 'Tipo: '.$pe->tipo.'<br>';
+    echo 'Nombre: '.$pe->nombre.'<br>';
+
+    //planes estudio
+    foreach ($pe->planes_estudio[0] as $i_ple => $plan) {
+        echo '--'.$plan->nombre.'<br>';
+        echo '--'.$plan->resolucion.'<br>';
+        echo '--'.$plan->fecha_registro.'<br>';
+    
+        //modulos
+        foreach ($plan->modulos_formativos[0] as $i_mod => $modulo) {
+            echo '----'.$modulo->descripcion.'<br>';
+         // echo '----'.$modulo->nro_modulo.'<br>';
+
+            //periodos
+            foreach ($modulo->periodos[0] as $i_per => $periodo) {
+                echo '------'.$periodo->descripcion.'<br>';
+
+                //unidades didacticas
+                foreach ($periodo->unidades_didacticas[0] as $i_uds => $unidades) {
+                    echo '--------'.$unidades->nombre.'<br>';
+                    echo '--------'.$unidades->creditos_teorico.'<br>';
+                    echo '--------'.$unidades->creditos_practico.'<br>';
+                    echo '--------'.$unidades->tipo.'<br>';
+                    echo '--------'.$unidades->horas_semanal.'<br>';
+                    echo '--------'.$unidades->horas_semestral.'<br>';
+                }
+             }
+        
+         }
+    }
+}
